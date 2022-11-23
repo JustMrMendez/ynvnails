499ebce398223ed8e0ae0f58a908f9c14805e048
Mr. Mendez <56850299+JustMrMendez@users.noreply.github.com>
2022-11-23 09:02:12 -0700

    Update Card.tsx

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,4 +1,4 @@
-function CardComponents(props: {
+function Card({
   img: string;
   title: string;
   description: string;
@@ -7,15 +7,15 @@ function CardComponents(props: {
     <>
       <div className="grid grid-cols-3 gap-4" />
       <div className="m-2 h-96 w-72 overflow-hidden rounded-3xl bg-pink-100">
-        <img src={props.img} alt="" className="rounded-3xl" />
+        <img src={img} alt="" className="rounded-3xl" />
         <div className="pl-2">
           <h3 className="text-xl font-semibold">
-            <p className="mb-2 leading-normal">{props.title}</p>
+            <p className="mb-2 leading-normal">{title}</p>
           </h3>
-          <p className="leading-normal text-gray-800">{props.description}</p>
+          <p className="leading-normal text-gray-800">{description}</p>
         </div>
       </div>
     </>
   );
 }
-export default CardComponents;
+export default Card;
