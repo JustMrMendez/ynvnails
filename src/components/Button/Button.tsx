3bed53b703a6224c784f2376d62ad17e347ccd2c
Richard Gonzalez <Pogo_33125@yahoo.com>
2022-11-30 19:17:23 -0500

    color change props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
   label: string;
+  color?: string;
+  labelColor: string;
 }
 
-function Button({ label }: ButtonProps) {
+function Button({ label, color, labelColor }: ButtonProps) {
   {
     return (
-      <button className="rounded-3xl bg-pink-600 py-3 px-8 font-bold text-white">
+      <button
+        className={`rounded-3xl py-3 px-8 font-bold text-${labelColor} bg-${color}`}
+      >
         {label}
       </button>
     );
