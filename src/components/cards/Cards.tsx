89bed5bc6295989725dcc052927eef7cc5f0617f
Richard Gonzalez <Pogo_33125@yahoo.com>
2022-12-07 19:47:27 -0500

    finish card with Product Logo

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -2,22 +2,28 @@ import Card from "./Card";
 
 function Cards() {
   return (
-    <div className="flex flex-col gap-5 md:flex-row">
-      <Card
-        href="https://picsum.photos/id/1/316/240"
-        title="New Year, New Styles"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />
-      <Card
-        href="https://picsum.photos/id/888/316/240"
-        title="Quatar 2022 Booked"
-        description="catch a flight to the 4geeks headquarters and learn about new programming languages."
-      />
-      <Card
-        href="https://picsum.photos/id/28/316/240"
-        title="Discover the wonders of react"
-        description="tailwinds & react working together. Lorem ipsum dolor sit amet, consectetur adip"
-      />
+    <div className="flex h-[75vh] max-w-5xl flex-col gap-5 md:flex-row">
+      <div className="md:w-1/2">
+        <Card
+          href="https://picsum.photos/id/1/316/240"
+          title="New Year, New Styles"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+      <div className="flex flex-col gap-5 md:h-1/2">
+        <Card
+          flip
+          href="https://picsum.photos/id/888/316/240"
+          title="Quatar 2022 Booked"
+          description="catch a flight to the 4geeks headquarters and learn about new programming languages."
+        />
+        <Card
+          flip
+          href="https://picsum.photos/id/28/316/240"
+          title="Discover the wonders of react"
+          description="tailwinds & react working together. Lorem ipsum dolor sit amet, consectetur adip"
+        />
+      </div>
     </div>
   );
 }
