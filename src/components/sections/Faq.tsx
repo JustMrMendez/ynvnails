88b76a3be42e1cd0025748ee1954820295d24b1f
Mercad0 <abimaelmercado05@gmail.com>
2022-12-15 19:40:11 -0500

    + faq for sections

--- src/components/sections/Faq.tsx
+++ src/components/sections/Faq.tsx
@@ -1,11 +1,23 @@
-function Faq() {
+function FAQ() {
   return (
-    <div>
-      <h1>FAQ</h1>
-      <p>
-        This is a list of questions about the project, including how to make a
-        question.
-      </p>
+    <div className="rounded-lg border p-4">
+      <h2 className="mb-2 text-xl font-bold">Frequently Asked Questions</h2>
+      <div className="mb-4">
+        <h3 className="mb-1 text-lg font-bold">
+          What are your Hoours of Operation
+        </h3>
+        <p className="text-gray-700">
+          We are open MONDAY through FRIDAY from 10:00 AM - 06:00 PM.
+        </p>
+      </div>
+      <div className="mb-4">
+        <h3 className="mb-1 text-lg font-bold">Do you accept walk in?</h3>
+        <p className="text-gray-700">
+          We do accept walk in but for us to provide a better service
+        </p>
+      </div>
     </div>
   );
 }
+
+export default FAQ;
