fafe4e1c9a9e8a80e35544c3f5ce1f7cf055d76b
Mr. Mendez <mrmendez@amvestor.com>
2022-12-23 19:57:53 -0500

    refractor

--- src/components/faqQA/FaqList.tsx
+++ src/components/faqQA/FaqList.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface faqlist {
   question: string;
   reply: string;
+  open?: boolean;
 }
 
 interface faqListProps {
@@ -10,20 +12,41 @@ interface faqListProps {
 }
 
 function FaqList({ faqList }: faqListProps) {
+  const [open, setOpen] = useState<Number[]>([]);
+
   return (
     <>
-      {faqList.map((faq, i) => (
-        <li className="relative justify-around text-center font-mono" key={i}>
-          {/* <svg
-            className="absolute h-7 w-7 overflow-visible rounded-full bg-pink-300 p-[8px] shadow-md shadow-pink-300 transition-all hover:scale-75 hover:bg-pink-400 hover:text-pink-600 hover:shadow-lg hover:shadow-pink-100"
-            viewBox="0 0 320 512"
+      <dl className="mt-5 flex max-w-2xl flex-col gap-5">
+        {faqList.map((faq, i) => (
+          <div
+            className=" justify-around rounded-xl bg-pink-50 p-5 font-mono shadow-md"
+            key={i}
           >
-            <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-          </svg> */}
-          <h2 className="m-8 text-xl text-pink-600">{faq.question}</h2>
-          <span className=" text-pink-400">{faq.reply}</span>
-        </li>
-      ))}
+            <dt className="mb-3 text-lg">
+              <button
+                onClick={() => setOpen(open.includes(i) ? [] : [i])}
+                type="button"
+                className="flex w-full items-start justify-between text-left text-gray-400"
+                aria-controls="faq-0"
+                aria-expanded="false"
+              >
+                <span className="text-xl text-pink-600">{faq.question}</span>
+                <span className="ml-6 flex h-7 items-center">
+                  <svg
+                    className="h-6 w-6 overflow-visible rounded-full bg-pink-300 p-1 shadow-md shadow-pink-300 transition-all hover:scale-75 hover:bg-pink-400 hover:text-pink-600 hover:shadow-lg hover:shadow-pink-100"
+                    viewBox="0 0 320 512"
+                  >
+                    <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                  </svg>
+                </span>
+              </button>
+            </dt>
+            <dd className={open.includes(i) ? "block" : "hidden"}>
+              <p className="text-pink-400">{faq.reply}</p>
+            </dd>
+          </div>
+        ))}
+      </dl>
     </>
   );
 }
--- src/views/Faq.tsx
+++ src/views/Faq.tsx
@@ -1,72 +1,69 @@
 import FaqList from "../components/faqQA/FaqList";
 
 function Faq() {
+  const faqList = [
+    {
+      question: "What types of manicures and pedicures do you offer?",
+      reply:
+        "we offer a variety of manicures and pedicures, such as basic polish changes, cuticle care, exfoliating scrubs, moisturizing treatments, and more.",
+    },
+    {
+      question: "Do you offer gel or acrylic nail services?",
+      reply:
+        "offer gel or acrylic nail services, which involve applying a synthetic material over the natural nail to create a more durable and long-lasting finish.",
+    },
+    {
+      question: "Do you have a wide range of polish colors to choose from?",
+      reply:
+        "Most nail salons have a wide range of polish colors to choose from, including classic shades, bold hues, and trendy shades.",
+    },
+    {
+      question: "Do you have any nail art or design options available?",
+      reply:
+        "Some salons offer nail art or design options, such as hand-painted designs, decals, or 3D accents.",
+    },
+    {
+      question: "Do you have any promotions or discounts currently available?",
+      reply:
+        "Many salons offer promotions or discounts at different times, such as first-time customer discounts or special deals for holidays or other occasions.",
+    },
+    {
+      question:
+        "Do you offer any additional services, such as waxing or massages?",
+      reply:
+        "At the moment we do not, but we can recommend you to the right place",
+    },
+    {
+      question:
+        "Do you have any sanitation protocols in place to prevent the spread of illness?",
+      reply:
+        "proper sanitation protocols in place to prevent the spread of illness, such as using disposable tools, sanitizing surfaces and equipment, and washing hands frequently.",
+    },
+    {
+      question:
+        "Do you have any policies on using your own polish or bringing in your own tools?",
+      reply:
+        "policies on using your own polish or bringing in your own tools, so it's a good idea to ask before bringing any of your own products.",
+    },
+    {
+      question: "do you offer free drinks?",
+      reply: "we have water and a variety of free drinks on sight",
+    },
+    {
+      question: "Do you have any customer loyalty programs?",
+      reply: "We are in the process of creating a customer loyalty program.",
+    },
+  ];
+
   return (
-    <div className="relative z-40 min-h-screen rounded-lg bg-pink-50">
+    <div className="relative z-40 min-h-screen  rounded-b-2xl bg-pink-100 pb-20 shadow-lg md:shadow-2xl">
       <div className="grid h-[30vh] w-full place-items-center rounded-b-lg bg-gradient-to-br from-purple-500 to-pink-600 shadow-lg">
         <h1 className="text-center text-4xl font-black text-white">
           Frequency Asked Questions
         </h1>
       </div>
-      <div className="mb-6">
-        <FaqList
-          faqList={[
-            {
-              question: "What types of manicures and pedicures do you offer?",
-              reply:
-                "we offer a variety of manicures and pedicures, such as basic polish changes, cuticle care, exfoliating scrubs, moisturizing treatments, and more.",
-            },
-            {
-              question: "Do you offer gel or acrylic nail services?",
-              reply:
-                "offer gel or acrylic nail services, which involve applying a synthetic material over the natural nail to create a more durable and long-lasting finish.",
-            },
-            {
-              question:
-                "Do you have a wide range of polish colors to choose from?",
-              reply:
-                "Most nail salons have a wide range of polish colors to choose from, including classic shades, bold hues, and trendy shades.",
-            },
-            {
-              question: "Do you have any nail art or design options available?",
-              reply:
-                "Some salons offer nail art or design options, such as hand-painted designs, decals, or 3D accents.",
-            },
-            {
-              question:
-                "Do you have any promotions or discounts currently available?",
-              reply:
-                "Many salons offer promotions or discounts at different times, such as first-time customer discounts or special deals for holidays or other occasions.",
-            },
-            {
-              question:
-                "Do you offer any additional services, such as waxing or massages?",
-              reply:
-                "At the moment we do not, but we can recommend you to the right place",
-            },
-            {
-              question:
-                "Do you have any sanitation protocols in place to prevent the spread of illness?",
-              reply:
-                "proper sanitation protocols in place to prevent the spread of illness, such as using disposable tools, sanitizing surfaces and equipment, and washing hands frequently.",
-            },
-            {
-              question:
-                "Do you have any policies on using your own polish or bringing in your own tools?",
-              reply:
-                "policies on using your own polish or bringing in your own tools, so it's a good idea to ask before bringing any of your own products.",
-            },
-            {
-              question: "do you offer free drinks?",
-              reply: "we have water and a variety of free drinks on sight",
-            },
-            {
-              question: "Do you have any customer loyalty programs?",
-              reply:
-                "We are in the process of creating a customer loyalty program.",
-            },
-          ]}
-        />
+      <div className="mb-6 grid w-full place-items-center">
+        <FaqList faqList={faqList} />
       </div>
     </div>
   );
