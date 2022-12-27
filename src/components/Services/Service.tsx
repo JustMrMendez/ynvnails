ede5508fc6cf5a32fa4006d0090c5868175b1baa
Mercad0 <abimaelmercado05@gmail.com>
2022-12-26 23:01:06 -0500

    - removed some padding from cards - fixed service layout

--- src/components/Services/Service.tsx
+++ src/components/Services/Service.tsx
@@ -1,13 +1,16 @@
 interface ServiceProps {
     title?: string;
-    description?: string;
+    description: { short: string; long: string };
     href?: string;
     flip?: boolean;
 }
 
 function Service({
     title = "Service #1",
-    description = " we provide Basic Manicure for cheap customers and the best Manicures with gold glitters.",
+    description = {
+        short: " we provide Basic Manicure for cheap customers and the best Manicures with gold glitters.",
+        long: " we provide Basic Manicure for cheap customers and the best Man",
+    },
     href = "https://picsum.photos/id/55/240/240",
     flip,
 }: ServiceProps) {
@@ -25,9 +28,14 @@ function Service({
                 />
                 <div className="absolute inset-0 z-0 m-auto h-full w-full rotate-45 rounded-2xl border-none bg-pink-300"></div>
             </div>
-            <div className="h-full w-full px-16 md:h-2/3 md:w-1/2">
-                <h1 className="text-5xl font-semibold"> {title}</h1>
-                <p className="mt-4 text-lg text-gray-600">{description}</p>
+            <div className="h-full w-full py-10 px-8 text-center md:h-2/3 md:w-1/2 md:px-16 md:text-left">
+                <h1 className="text-3xl font-semibold md:text-5xl"> {title}</h1>
+                <p className="mt-2 hidden text-xl text-gray-600 md:mt-4 md:block md:text-lg">
+                    {description.long}
+                </p>
+                <p className="mt-2 text-xl text-gray-600 md:mt-4 md:hidden md:text-lg">
+                    {description.short}
+                </p>
             </div>
         </div>
     );
--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,31 +1,31 @@
 import Card from "./Card";
 
 function Cards() {
-  return (
-    <div className="flex max-h-full max-w-5xl flex-col gap-5 md:flex-row">
-      <div className="md:w-1/2">
-        <Card
-          href="https://picsum.photos/id/1/316/240"
-          title="New Year, New Styles"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-      </div>
-      <div className="flex flex-col gap-5 md:h-1/2">
-        <Card
-          flip
-          href="https://picsum.photos/id/888/316/240"
-          title="Quatar 2022 Booked"
-          description="catch a flight to the 4geeks headquarters and learn about new programming languages."
-        />
-        <Card
-          flip
-          href="https://picsum.photos/id/28/316/240"
-          title="Discover the wonders of react"
-          description="tailwinds & react working together. Lorem ipsum dolor sit amet, consectetur adip"
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex max-h-full max-w-5xl flex-col gap-5 md:flex-row">
+            <div className="md:w-1/2">
+                <Card
+                    href="https://picsum.photos/id/1/316/240"
+                    title="New Year, New Styles"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                />
+            </div>
+            <div className="flex flex-col gap-5 md:h-1/2">
+                <Card
+                    flip
+                    href="https://picsum.photos/id/888/316/240"
+                    title="Qatar 2022 Booked"
+                    description="catch a flight to the 4geeks headquarters and learn about new programming languages."
+                />
+                <Card
+                    flip
+                    href="https://picsum.photos/id/28/316/240"
+                    title="Discover the wonders of react"
+                    description="tailwinds & react working together. Lorem ipsum dolor sit amet, consectetur adip"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Cards;
