ca2622ed1ffb53fb944a436eb0633f854d719c6b
Mr. Mendez <mrmendez@amvestor.com>
2022-12-26 21:16:11 -0500

    pre-fix

--- src/components/Services/Service.tsx
+++ src/components/Services/Service.tsx
@@ -1,36 +1,36 @@
 interface ServiceProps {
-  title?: string;
-  description?: string;
-  href?: string;
-  flip?: boolean;
+    title?: string;
+    description?: string;
+    href?: string;
+    flip?: boolean;
 }
 
 function Service({
-  title = "Service #1",
-  description = " we provide Basic Manicure for cheap customers and the best Manicures with gold glitters.",
-  href = "https://picsum.photos/id/55/240/240",
-  flip,
+    title = "Service #1",
+    description = " we provide Basic Manicure for cheap customers and the best Manicures with gold glitters.",
+    href = "https://picsum.photos/id/55/240/240",
+    flip,
 }: ServiceProps) {
-  return (
-    <div
-      className={`my-10 flex h-4/5 w-full flex-col items-center justify-around gap-6 md:h-2/5 ${
-        flip ? "md:flex-row-reverse" : "md:flex-row"
-      }`}
-    >
-      <div className="relative aspect-square h-4/5 max-h-[16rem] md:max-h-[20rem]">
-        <img
-          src={href}
-          alt=""
-          className="absolute top-0 z-10 h-full w-full rounded-xl"
-        />
-        <div className="absolute z-0 h-full w-full rotate-45 rounded-2xl border-none bg-pink-300"></div>
-      </div>
-      <div className="h-full w-full px-16 md:h-2/3 md:w-1/2">
-        <h1 className="text-5xl font-semibold"> {title}</h1>
-        <p className="mt-4 text-lg text-gray-600">{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={`my-10 flex h-4/5 w-full flex-col items-center justify-around gap-6 md:h-2/5 ${
+                flip ? "md:flex-row-reverse" : "md:flex-row"
+            }`}
+        >
+            <div className="relative aspect-square md:max-h-[20rem]">
+                <img
+                    src={href}
+                    alt=""
+                    className="sticky z-10 h-full w-full rounded-xl shadow-md drop-shadow-xl"
+                />
+                <div className="absolute inset-0 z-0 m-auto h-full w-full rotate-45 rounded-2xl border-none bg-pink-300"></div>
+            </div>
+            <div className="h-full w-full px-16 md:h-2/3 md:w-1/2">
+                <h1 className="text-5xl font-semibold"> {title}</h1>
+                <p className="mt-4 text-lg text-gray-600">{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default Service;
--- src/main.tsx
+++ src/main.tsx
@@ -7,19 +7,22 @@ import Faq from "./views/Faq";
 import Navbar from "./components/navigation/Navbar";
 import Footer from "./components/footer/Footer";
 import Booking from "./views/bookin";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 ReactDOM.render(
-    <Router>
-        <div className="min-w-screen grid place-items-center">
-            <Navbar />
-        </div>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/product/:productId" element={<Product />} />
-            <Route path="/faq" element={<Faq />} />
-            <Route path="/booking" element={<Booking />} />
-        </Routes>
-        <Footer />
-    </Router>,
+    <UserAuthContextProvider>
+        <Router>
+            <div className="min-w-screen grid place-items-center">
+                <Navbar />
+            </div>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/product/:productId" element={<Product />} />
+                <Route path="/faq" element={<Faq />} />
+                <Route path="/booking" element={<Booking />} />
+            </Routes>
+            <Footer />
+        </Router>
+    </UserAuthContextProvider>,
     document.getElementById("root")
 );
