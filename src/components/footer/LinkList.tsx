dbaf9b19aafa1198d35cebbdddbb8d46434522ad
Mercad0 <abimaelmercado05@gmail.com>
2023-01-22 22:25:47 -0500

    FAQ will load to the top instead of loading the page at the bottom.

--- src/components/footer/LinkList.tsx
+++ src/components/footer/LinkList.tsx
@@ -17,7 +17,7 @@ function Links({ linkList }: linkListProps) {
                     key={link.title}
                     className="justify-around text-center hover:scale-110"
                 >
-                    <Link to={link.href}>
+                    <Link to={link.href} onClick={() => window.scrollTo(0, 0)}>
                         <span className="text-sm text-gray-600 hover:text-pink-600 md:text-base">
                             {link.title}
                         </span>
